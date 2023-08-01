7f6af826a1ffda16dbab624ee4ade77b7a01b04f
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-08-01 21:38:02 +0300

    seems work

---
+++ src/js/cat-api.js
@@ -0,0 +1,33 @@
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] = 'live_prv4XOu3leUjObzOAVoKovC3ofskoeEGLskxizVlxsqKGtyA0btM61dS7Q76ICHU';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+
+function fetchBreeds() {
+  return axios
+    .get(`breeds/`)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    })
+    .catch(() => {
+      Report.failure('Oops! Something went wrong!');
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data[0];
+    })
+    .catch(() => {
+      Report.failure('Oops! Something went wrong! ');
+    });
+}
+export { fetchBreeds, fetchCatByBreed };
---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import { Loading, Report } from 'notiflix';
+
+const elements = {
+  breedSelect: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info')
+};
+
+elements.breedSelect.classList.add('hidden');
+Loading.standard('Loading data, please wait...');
+
+const createMarkupCatInfo = (cat) => `
+  <div class="container">
+    <div class="thumb-pic">
+      <img src="${cat.url}" alt="${cat.id}" height="350" />
+    </div>
+    <div class="thumb">
+      <h1>${cat.breeds[0].name}</h1>
+      <p>${cat.breeds[0].description}</p>
+      <p><b>Temperament:</b> ${cat.breeds[0].temperament}</p>
+    </div>
+  </div>`;
+
+const handleBreedSelectChange = (evt) => {
+  evt.preventDefault();
+  elements.catInfo.style.display = 'none'; 
+  Loading.standard('Loading data, please wait...');
+
+  const breedSelectId = elements.breedSelect.value;
+
+  fetchCatByBreed(breedSelectId)
+    .then(cat => {
+      Loading.remove();
+      elements.catInfo.innerHTML = createMarkupCatInfo(cat);
+      elements.catInfo.style.display = 'block'; 
+    })
+    .catch(error => {
+      console.error(error);
+      Report.failure('Oops! Something went wrong!');
+      Loading.remove();
+      elements.catInfo.style.display = 'block'; 
+    });
+};
+
+fetchBreeds()
+  .then(data => {
+    const option = data.map(({ id, name }) => `<option value="${id}">${name}</option>`);
+    elements.breedSelect.innerHTML = option;
+    Loading.remove();
+    elements.breedSelect.style.display = 'block'; 
+  })
+  .catch(() => {
+    Report.failure('Oops! Something went wrong!');
+    Loading.remove();
+    elements.breedSelect.style.display = 'block'; 
+  });
+
+elements.breedSelect.addEventListener('change', handleBreedSelectChange);
